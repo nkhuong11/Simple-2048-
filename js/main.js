6c5e853d65593a121d3de90844e2d74fba7fe9de
Khuong Nguyen <khuong.nguyen0322@gmail.com>
2019-08-11 21:02:37 +0700

    2048 noob version

--- js/main.js
+++ js/main.js
@@ -365,18 +365,17 @@ window.onkeydown = function(event){
 };
 
 window.onload = function(){
-  var screen_height = window.screen.height;
-  var screen_width = window.screen.width;
+  // var screen_height = window.screen.height;
+  // var screen_width = window.screen.width;
   var menu_element = document.getElementById("menu");
   var grid_container = document.getElementById("grid-container");
-  if (screen_height < screen_width) {
-    grid_container.style.height = (screen_width * 0.95) + "px";
-    grid_container.style.width = grid_container.style.height;
-    // grid_container.style.height = (screen_height * 0.73) + "px";
-    // grid_container.style.width = grid_container.style.height;
-  } else {
-   
-  }
+  // if (screen_height < screen_width) {
+  //   grid_container.style.height = (screen_height * 0.73) + "px";
+  //   grid_container.style.width = grid_container.style.height;
+  // } else {
+  //   grid_container.style.height = (screen_width * 0.95) + "px";
+  //   grid_container.style.width = grid_container.style.height;
+  // }
   
   grid_container.addEventListener('touchstart',touchStart,{passive : true});
   grid_container.addEventListener('touchend',touchEnd,{passive : true});
